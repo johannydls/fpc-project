d6011e5eb6f3865939b4bc2f2b59ccffa4bbe4b8
johannydls <johanny.dls@gmail.com>
2017-06-30 11:06:57 -0300

    Limpando codigos

---
+++ milestone2/README.md
@@ -0,0 +1,8 @@
+# fpc-project
+Projeto da disciplina Fundamentos de Programação Concorrente - 2017.1
+
+#Milestone 1
+
+Inicie o servidor: node server.js
+
+Em seguida, inicie o cliente, que irá fazer a requisição: node client.js
--- milestone2/server.js
+++ milestone2/server.js
@@ -1,19 +1,20 @@
 'use strict';
 
 let http = require('http')
-  , url = require('url')
   , fs = require('fs');
 
 const port = 3000;
 
 http.createServer(function(request, response) {
 	
-	var result = url.parse(request.url, true);
 	response.setHeader("method",request.method);
 	response.setHeader("URL",request.url);
 	response.setHeader("httpVersion","HTTP/" + request.httpVersion);
 
 	response.writeHead(200, {'Content-Type':'text/html'});
+
+	console.log("Client request: " + request.method + " " + request.url + " HTTP/" + 
+		request.httpVersion + " " + response.statusCode + " " + response.statusMessage);
 
 	fs.readFile('./index.html', function(err, file) {
 		if (err) console.log("Erro ao ler arquivo.");
@@ -21,8 +22,6 @@ http.createServer(function(request, response) {
 		response.end(file);
 
 	});
-
-	//response.end();
 
 }).listen(port, function() {
 	console.log('Servidor rodando em localhost:'+port);
